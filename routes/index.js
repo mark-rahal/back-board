bf32eaa925bd7a07d6a3a36f10d5885ec7a6b5d4
mark <mgr0904@gmail.com>
2019-03-13 18:14:44 -0700

    Removed async module

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const router = express.Router();
 const db = require('./../db');
-const async = require('async');
 
 /* GET home page. */
 router.get('/', function (req, res) {
